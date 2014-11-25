d3d1ecba2cad81668f174753cf0152deff8297fd
Taras Kalapun <t.kalapun@gmail.com>
2014-11-25 13:41:24 +0100

    Working impl

--- pos_cases.js
+++ pos_cases.js
@@ -1,23 +1,42 @@
+var path = require('path'),
+    fs = require('fs'),
+    yaml = require('js-yaml');
+
 module.exports = {
   cases: {},
+
+  unloadAll: function() {
+    this.cases = {}
+  },
+
+  loadCase: function(case_name) {
+    var casePath = path.join(__dirname, 'cases');
+
+    var file = case_name.toString();
+    if (path.extname(file) != '.yml') {
+      file = file + '.yml'
+    }
+
+    var key = path.basename(file, path.extname(file))
+
+    try {
+      var filename = path.join(casePath, file),
+          contents = fs.readFileSync(filename, 'utf8'),
+          data     = yaml.load(contents);
+      this.cases[key.toString()] = data
+    } catch (err) {
+      console.log(err.stack || String(err));
+    }
+  },
+
   loadAll: function() {
-    var cases = {};
     var casePath = path.join(__dirname, 'cases');
     var files = fs.readdirSync(casePath);
     for (k in files) {
       var file = files[k]
-      try {
-        var filename = path.join(casePath, file),
-            contents = fs.readFileSync(filename, 'utf8'),
-            data     = yaml.load(contents);
-        var key = path.basename(file, path.extname(file))
-        cases[key.toString()] = data
-      } catch (err) {
-        console.log(err.stack || String(err));
-      }
+      this.loadCase(file);
     }
-    console.log("Loaded %d cases", Object.keys(cases).length);
-    this.cases = cases;
-    return cases;
+    console.log("Loaded %d cases", Object.keys(this.cases).length);
+    return this.cases;
   }
 }
